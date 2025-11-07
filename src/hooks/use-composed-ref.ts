981305ab8f4a318a4f9e5702101f97dbb04cfd7b
Tuan Nguyen <ntuan.2502@gmail.com>
2025-11-07 09:31:14 +0700

    refactor(hooks): simplify useComposedRef logic

--- src/hooks/use-composed-ref.ts
+++ src/hooks/use-composed-ref.ts
@@ -1,6 +1,6 @@
 "use client"
 
-import { useCallback, useRef } from "react"
+import { useCallback } from "react"
 
 // basically Exclude<React.ClassAttributes<T>["ref"], string>
 type UserRef<T> =
@@ -22,19 +22,11 @@ export const useComposedRef = <T extends HTMLElement>(
   libRef: React.RefObject<T | null>,
   userRef: UserRef<T>
 ) => {
-  const prevUserRef = useRef<UserRef<T>>(null)
-
   return useCallback(
     (instance: T | null) => {
-      if (libRef && "current" in libRef) {
-        ;(libRef as { current: T | null }).current = instance
+      if (libRef) {
+        updateRef(libRef, instance)
       }
-
-      if (prevUserRef.current) {
-        updateRef(prevUserRef.current, null)
-      }
-
-      prevUserRef.current = userRef
 
       if (userRef) {
         updateRef(userRef, instance)
